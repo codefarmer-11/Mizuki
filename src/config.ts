e27448abe69454baf58f36246bdaaf56b6a5b118
codefarmer-11 <13051873270@163.com>
2026-04-03 13:55:26 +0800

    logo

--- src/config.ts
+++ src/config.ts
@@ -57,7 +57,7 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "MoonL",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
-		icon: "assets/home/home.webp",
+		icon: "assets/home/Moon.png",
 		// 网站Logo图片路径
 		logo: "assets/home/default-logo.webp",
 	},
