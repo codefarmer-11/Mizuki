b9672b019e96f671b5fe65c32a80d07b96f0b8fe
codefarmer-11 <13051873270@163.com>
2026-04-09 19:12:55 +0800

    更新翻译

--- src/config.ts
+++ src/config.ts
@@ -445,6 +445,7 @@ export const shareConfig: ShareConfig = {
 	enable: true, // 启用分享功能
 };
 
+
 export const announcementConfig: AnnouncementConfig = {
 	title: "", // 公告标题，填空使用i18n字符串Key.announcement
 	content: "这是一个公告", // 公告内容
